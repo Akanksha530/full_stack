cd320d7fbb20e06001df354a618fc1f0dacdb094
Akanksha <akankshasingh47804@gmail.com>
2025-01-15 15:13:46 +0530

    `Added various file operations using fs module: open, writeFile, readFile, and append.`

--- Sem_6/lab_01/index.js
+++ Sem_6/lab_01/index.js
@@ -1,2 +1,40 @@
 const fs = require('fs');
+/*
+//write flag,w 
+var wf = fs.open('mydata.txt','w',(err)=>{
+    if(err) console.log(err);
+    return;
+});
+console.log("File created");
 
+//read flag,r 
+var wf = fs.open('mydata.txt','r',(err)=>{
+    if(err) console.log(err);
+    return;
+});
+
+//append flag,a
+var wf = fs.open('mydata.txt','a',(err)=>{
+    if(err) console.log(err);
+    return;
+});
+//append flag,a
+var wf = fs.open('myd.txt','a',(err)=>{
+    if(err) console.log(err);
+    return;
+});
+
+//X: create a new file if it does not exist
+*/
+//writeFile function
+var wf = fs.writeFile('data.txt','Akanksha',(err)=>{
+    if(err) console.log(err);
+    return;
+});
+
+//readFile function
+var wf = fs.readFile('data.txt','utf8',(err,data)=>{
+    if(err) console.log(err);
+    else console.log(data);
+    return;
+});
